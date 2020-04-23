9756559bfb35082e7bbc42ac82660b16471bc12d
Nikita Ravindran <ravindran.n@husky.neu.edu>
2020-04-22 21:10:25 -0500

    procfile and setup.sh for heroku

--- Video_Games_Streaming_Analytics/Model/setup.sh
+++ Video_Games_Streaming_Analytics/Model/setup.sh
@@ -1,11 +1,8 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"nikitaravi94@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
-port = $PORT\n\
+\n\
 " > ~/.streamlit/config.toml
